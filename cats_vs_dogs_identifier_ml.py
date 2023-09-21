94f88c03f21f39de55accbed6f716219ab57fd67
t0mwelsh <139456825+t0mwelsh@users.noreply.github.com>
2023-09-21 15:49:27 +0100

    Added software and a locally trained model

---
+++ cats_vs_dogs_identifier_ml.py
@@ -0,0 +1,158 @@
+import tensorflow.compat.v1 as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+# DATA FROM https://cdn.freecodecamp.org/project-data/cats-and-dogs/cats_and_dogs.zip
+PATH = 'cats_and_dogs'
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+test_dir = os.path.join(PATH, 'test')
+
+def plotImages(images_arr, probabilities = False, save=False):
+    fig, axes = plt.subplots(len(images_arr), 1, figsize=(5,len(images_arr) * 3))
+    if probabilities is False:
+      for img, ax in zip( images_arr, axes):
+          ax.imshow(img)
+          ax.axis('off')
+    else:
+      for img, probability, ax in zip( images_arr, probabilities, axes):
+          ax.imshow(img)
+          ax.axis('off')
+          if probability > 0.5:
+              ax.set_title("%.2f" % (probability*100) + "% dog")
+          else:
+              ax.set_title("%.2f" % ((1-probability)*100) + "% cat")
+    
+    if save:
+        current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+        fig.savefig(f"prediction_fig_{current_time}.png")
+    
+    plt.show()
+
+# Variables for pre-processing and training.
+batch_size = 128
+epochs = 20
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+## LOAD DATA
+train_image_generator = ImageDataGenerator(
+    rescale=1./255, 
+    rotation_range=40, # random data augmentation
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    fill_mode="nearest")
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+test_image_generator       = ImageDataGenerator(rescale=1./255)
+
+train_data_gen = train_image_generator.flow_from_directory(
+                    train_dir,
+                    target_size=(IMG_HEIGHT, IMG_WIDTH),
+                    batch_size=batch_size,
+                    class_mode='binary') #two classes
+
+val_data_gen   = validation_image_generator.flow_from_directory(
+                    validation_dir,
+                    target_size=(IMG_HEIGHT, IMG_WIDTH),
+                    batch_size=batch_size,
+                    class_mode='binary')
+
+test_data_gen  = test_image_generator.flow_from_directory(
+                    PATH,
+                    target_size=(IMG_HEIGHT, IMG_WIDTH),
+                    batch_size=50, #number of test samples
+                    classes=['test'], #due to directory structure
+                    class_mode=None, #return only the images
+                    shuffle=False) #need to yield the images in order to match with unique ids
+
+total_train = train_data_gen.n
+total_val = val_data_gen.n
+total_test = test_data_gen.n
+
+# Create the model
+#Convolutional Base
+model = Sequential()
+model.add(Input(shape=(IMG_HEIGHT, IMG_WIDTH, 3)))
+model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Dense layers
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='sigmoid')) # 1 = dog, 0 = cat
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy']
+)
+
+history = model.fit(train_data_gen,
+          steps_per_epoch=int(round(total_train / batch_size)),
+          epochs=epochs,
+          validation_data=val_data_gen,
+          validation_steps=int(round(total_val / batch_size))
+)
+
+# check accuracy
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+# check with test data
+probabilities = model.predict(test_data_gen).flatten()
+
+answers =  [1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0,
+            1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0,
+            1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
+            1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 
+            0, 0, 0, 0, 0, 0]
+
+correct = 0
+
+for probability, answer in zip(probabilities, answers):
+  if round(probability) == answer:
+    correct +=1
+
+percentage_identified = (correct / len(answers)) * 100
+
+testing_images = next(test_data_gen)
+plotImages(testing_images, probabilities, True)
+
+model.save(f"cats_vs_dogs_model_{percentage_identified}%.h5")
